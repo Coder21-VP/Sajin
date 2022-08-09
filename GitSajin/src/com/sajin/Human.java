74b05701e4caa415ced59b5d098c5d0e7a382dad
VishnuV-21 <vishnuprakash2110@gmail.com>
2022-08-09 18:01:59 +0530

    first commit-getting name form user

---
+++ GitSajin/src/com/sajin/Human.java
@@ -0,0 +1,17 @@
+package com.sajin;
+
+import java.util.Scanner;
+
+public class Human {
+static String name;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		name=sc.nextLine();
+		System.out.println("MY name is "+name);
+
+	}
+
+}
